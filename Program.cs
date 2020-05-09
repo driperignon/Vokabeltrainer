0f76b4987548e5a335dbbd9de54f33ed591a0d05
Joshua Ebert <selyx666@gmail.com>
2020-05-09 19:08:13 +0200

    Test commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/**
+ * File made by Joshua
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,48 +11,49 @@ using System.Windows.Forms;
 
 namespace Vokabeltrainer
 {
-	class Program
-	{
-		[STAThreadAttribute()]
-		static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
-			Console.WriteLine("1. Lernen\n2. Vokabeln zum Lernen vorbereiten\n3. Beenden");
-			switch(Console.ReadLine())
-			{
-				case "1":
-					Console.Clear();
-					Console.WriteLine("1. Deutsch nach Englisch\n2. Englisch nach Deutsch\n3. Gemischt\n4. Beenden");
-					switch(Console.ReadLine())
-					{
-						case "1":
-							Application.Run(new Lernen(1));
-							break;
+    internal class Program
+    {
+        [STAThreadAttribute()]
+        private static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Console.WriteLine("1. Lernen\n2. Vokabeln zum Lernen vorbereiten\n3. Beenden");
+            //Test
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("1. Deutsch nach Englisch\n2. Englisch nach Deutsch\n3. Gemischt\n4. Beenden");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Application.Run(new Lernen(1));
+                            break;
 
-						case "2":
-							Application.Run(new Lernen(2));
-							break;
+                        case "2":
+                            Application.Run(new Lernen(2));
+                            break;
 
-						case "3":
-							Application.Run(new Lernen(3));
-							break;
+                        case "3":
+                            Application.Run(new Lernen(3));
+                            break;
 
-						default:
-						case "4":
-							Application.Exit();
-							break;
-					}
-					break;
+                        default:
+                        case "4":
+                            Application.Exit();
+                            break;
+                    }
+                    break;
 
-				case "2":
-					Application.Run(new Vorbereiten());
-					break;
+                case "2":
+                    Application.Run(new Vorbereiten());
+                    break;
 
-				default:
-				case "3":
-					Application.Exit();
-					break;
-			}
-		}
-	}
+                default:
+                case "3":
+                    Application.Exit();
+                    break;
+            }
+        }
+    }
 }
